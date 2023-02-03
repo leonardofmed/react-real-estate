e4975f927ade7d4b3131ee8c75fce83ec3b8a0d0
Leonardo <leonardo.fmed@hotmail.com>
2023-02-03 07:49:19 -0300

    Fetch API and home page initial structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bayut-production.s3.eu-central-1.amazonaws.com',
+        port: ''
+      }
+    ]
+  },
+  env: {
+    BAYUT_API_KEY: 'a34d19b454msha88cf41bca3a42ap1e1244jsnd1d375cc711b'
+  }
 }
 
 module.exports = nextConfig
